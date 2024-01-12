680140d2c588151af8a8d52ddedd4eecf999d63e
Myriam1501 <myriamo93500@gmail.com>
2024-01-12 18:19:35 +0100

    fs

--- wechat-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/GivingServiceImpl.java
+++ wechat-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/service/GivingServiceImpl.java
@@ -17,62 +17,9 @@ public class GivingServiceImpl implements GivingService {
 
     @PersistenceContext
     EntityManager em;
-<<<<<<< HEAD
 
     @Override
     @Transactional
-=======
-/*
-
-    @Override
-    @Transactional
-    public Booking book(Booking give) throws UnsuficientQuotaForVenueException {
-        try {
-            VenueQuota vq = (VenueQuota) (em.createQuery("select q from VenueQuota q where q.id.venue.id=:venueId and q.id.vendor.id=:vendorId and q.seatingQuota>=:countSeating and q.standingQuota>=:countStanding")
-                    .setParameter("vendorId", give.getVendorId())
-                    .setParameter("venueId", give.getVenueId())
-                    .setParameter("countStanding", give.getStandingTicketsNumber())
-                    .setParameter("countSeating", give.getSeatingTicketsNumber()).getSingleResult());
-            vq.setSeatingQuota(vq.getSeatingQuota() - give.getSeatingTicketsNumber());
-            vq.setStandingQuota(vq.getStandingQuota() - give.getStandingTicketsNumber());
-
-            Venue venue = em.find(Venue.class, give.getVenueId());
-            Vendor vendor = em.find(Vendor.class, give.getVendorId());
-
-
-            for (int i = 0; i < give.getStandingTicketsNumber(); i++) {
-                Ticket ticket = new Ticket();
-                ticket.setValidUntil(Instant.now().plus(10, ChronoUnit.HOURS));
-                ticket.setIdVendor(vendor);
-                ticket.setIdVenue(venue);
-                em.persist(ticket);
-                give.getStandingTransitionalTicket().add(ticket.getId());
-
-            }
-
-            for (int i = 0; i < give.getSeatingTicketsNumber(); i++) {
-                Ticket ticket = new Ticket();
-                ticket.setValidUntil(Instant.now().plus(10, ChronoUnit.MINUTES));
-                ticket.setIdVendor(vendor);
-                ticket.setIdVenue(venue);
-                ticket.setSeatReference("");
-                em.persist(ticket);
-
-                give.getSeatingTransitionalTicket().add(ticket.getId());
-
-            }
-
-        } catch (NonUniqueResultException | NoResultException e) {
-            throw new UnsuficientQuotaForVenueException(give.getVenueId());
-        }
-        return give;
-
-
-    }
-*/
-    @Override
-    @Transactional
->>>>>>> a4244aa7616f8a31d39ca3cf452225aea48efc06
     public Giving giveMoney(Giving give) throws UnsuficientQuotaForVenueException {
         
         return null;
@@ -81,7 +28,7 @@ public class GivingServiceImpl implements GivingService {
     @Override
     @Transactional
     public Giving giveTime(Giving give) throws UnsuficientQuotaForVenueException {
-<<<<<<< HEAD
+
        /* try {
 
 
@@ -105,12 +52,7 @@ public class GivingServiceImpl implements GivingService {
             throw new UnsuficientQuotaForVenueException(give.getVenueId());
         }
         return give;*/
-<<<<<<< HEAD
             return null;
-=======
-        return null;
-
->>>>>>> a4244aa7616f8a31d39ca3cf452225aea48efc06
 
     }
 
