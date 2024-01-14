450dd52491b37bb0d321131a7f1003a9e35a0bb0
emma <chen.emma1001@gmail.com>
2024-01-14 14:33:20 +0100

    new clean

--- wechat-government/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/AlertGateway.java
+++ wechat-government/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/AlertGateway.java
@@ -2,23 +2,15 @@ package fr.pantheonsorbonne.ufr27.miage.camel;
 
 import fr.pantheonsorbonne.ufr27.miage.dto.Alert;
 import fr.pantheonsorbonne.ufr27.miage.model.Event;
-import fr.pantheonsorbonne.ufr27.miage.service.AlertService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import java.io.IOException;
 
 @ApplicationScoped
 public class AlertGateway {
-    @Inject
-    AlertService AlertService;
-
-    @ConfigProperty(name = "fr.pantheonsorbonne.ufr27.miage.jmsPrefix")
-    String jmsPrefix;
-
     @Inject
     CamelContext camelContext;
 
