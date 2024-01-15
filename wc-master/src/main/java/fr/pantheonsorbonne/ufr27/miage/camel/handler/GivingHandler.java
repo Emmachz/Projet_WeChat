513d520ddfefff5ab6a8f158a4c99ae4b38f053c
Myriam1501 <myriamo93500@gmail.com>
2024-01-15 08:12:10 +0100

    clean fix

--- wc-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/handler/GivingHandler.java
+++ wc-master/src/main/java/fr/pantheonsorbonne/ufr27/miage/camel/handler/GivingHandler.java
@@ -10,7 +10,9 @@ public class GivingHandler {
 
     @Handler
     public void checkTypeOfGive(Exchange exchange) {
+
             Giving give = exchange.getMessage().getBody(Giving.class);
+            System.out.println("edz fevh" + give.getTypeGive());
             exchange.getMessage().setHeader("typeGive", give.getTypeGive());
             exchange.getMessage().setHeader("login", give.getUserLogin());
             if(give.getUsergive().wallet() >= give.getQuantity()){
